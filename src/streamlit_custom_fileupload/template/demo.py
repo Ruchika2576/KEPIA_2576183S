046fe57980c6418487e83294ddc0e4878e3a03f4
Ruchika Singhi <ruchikasinghi@pc-7-195.customer.ask4.lan>
2021-12-02 17:03:19 +0000

    removing custom file uploader

--- src/streamlit_custom_fileupload/template/demo.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from custom_fileupload import st_custom_fileupload
-
-# components.html("<html><body><h1>Hello, World!! how to render a component from streamlit</h1></body></html>", width=200, height=200)
-st.title("Testing")
-st_custom_fileupload()
